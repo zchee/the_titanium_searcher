02d6dead4c32931c0bdbc51c16b5ec3d7cedb0d8
Koichi Shiraishi <zchee.io@gmail.com>
2017-06-20 11:05:57 +0900

    internal/mmap: add mmap benchmark
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

---
+++ internal/mmap/mmap_bench_test.go
@@ -0,0 +1,97 @@
+// Copyright 2017 The the_titanium_searcher. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package mmap
+
+import (
+	"bufio"
+	"os"
+	"testing"
+)
+
+const (
+	smallFilename = "mmap_test.go"
+	medFilename   = "testdata/Lint.cpp" // TODO(zchee): fetch medium size(about 50000) file
+	hugeFilename  = "testdata/huge.txt" // TODO(zchee): create huge.txt use `dd`
+)
+
+var (
+	smallFilesize int64
+	medFilesize   int64
+	hugeFilesize  int64
+)
+
+func TestMain(m *testing.M) {
+	// for Benchmark SetBytes.
+	smallFilesize = readSize(smallFilename)
+	medFilesize = readSize(medFilename)
+	hugeFilesize = readSize(hugeFilename)
+
+	os.Exit(m.Run())
+}
+
+// NOTE(zchee): If size is about 50000 or higher, mmap.Open is faster than read(2)
+// We will use `1024<<6`(65536) for the switching to mmap(2) or read(2).
+func benchmarkOpen(b *testing.B, filename string, size int64) {
+	b.SetBytes(size)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		if _, err := Open(filename); err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+func benchmarkRead(b *testing.B, filename string, size int64) {
+	b.SetBytes(size)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		f, err := os.Open(filename)
+		if err != nil {
+			b.Fatal(err)
+		}
+		fi, err := f.Stat()
+		if err != nil {
+			b.Fatal(err)
+		}
+		buf := make([]byte, 0, fi.Size())
+		_, err = f.Read(buf)
+		if err != nil {
+			b.Fatal(err)
+		}
+		f.Close()
+	}
+}
+
+func benchmarkBufioRead(b *testing.B, filename string, size int64) {
+	b.SetBytes(size)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		f, err := os.Open(filename)
+		if err != nil {
+			b.Fatal(err)
+		}
+		fi, err := f.Stat()
+		if err != nil {
+			b.Fatal(err)
+		}
+		reader := bufio.NewReaderSize(f, int(fi.Size()))
+		buf := make([]byte, 0, fi.Size())
+		_, err = reader.Read(buf)
+		if err != nil {
+			b.Fatal(err)
+		}
+		f.Close()
+	}
+}
+
+func BenchmarkSmallOpen(b *testing.B)       { benchmarkOpen(b, smallFilename, smallFilesize) }
+func BenchmarkSmallRead(b *testing.B)       { benchmarkRead(b, smallFilename, smallFilesize) }
+func BenchmarkSmallBufioRead(b *testing.B)  { benchmarkBufioRead(b, smallFilename, smallFilesize) }
+func BenchmarkMediumOpen(b *testing.B)      { benchmarkOpen(b, medFilename, medFilesize) }
+func BenchmarkMediumRead(b *testing.B)      { benchmarkRead(b, medFilename, medFilesize) }
+func BenchmarkMediumBufioRead(b *testing.B) { benchmarkBufioRead(b, medFilename, medFilesize) }
+func BenchmarkHugeOpen(b *testing.B)        { benchmarkOpen(b, hugeFilename, hugeFilesize) }
+func BenchmarkHugeRead(b *testing.B)        { benchmarkRead(b, hugeFilename, hugeFilesize) }
+func BenchmarkHugeBufioRead(b *testing.B)   { benchmarkBufioRead(b, hugeFilename, hugeFilesize) }
--- internal/mmap/mmap_test.go
+++ internal/mmap/mmap_test.go
@@ -8,12 +8,25 @@ import (
 	"bytes"
 	"io"
 	"io/ioutil"
+	"os"
 	"testing"
 )
 
+func readSize(filename string) int64 {
+	f, err := os.Open(filename)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	fi, err := f.Stat()
+	if err != nil {
+		panic(err)
+	}
+	return fi.Size()
+}
+
 func TestOpen(t *testing.T) {
-	const filename = "mmap_test.go"
-	r, err := Open(filename)
+	r, err := Open(smallFilename)
 	if err != nil {
 		t.Fatalf("Open: %v", err)
 	}
@@ -21,7 +34,7 @@ func TestOpen(t *testing.T) {
 	if _, err := r.ReadAt(got, 0); err != nil && err != io.EOF {
 		t.Fatalf("ReadAt: %v", err)
 	}
-	want, err := ioutil.ReadFile(filename)
+	want, err := ioutil.ReadFile(smallFilename)
 	if err != nil {
 		t.Fatalf("ioutil.ReadFile: %v", err)
 	}
