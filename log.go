50cc5396ae5a36805379c600b3569fa7f4326a39
Koichi Shiraishi <zchee.io@gmail.com>
2017-06-24 00:54:05 +0900

    log: remove _debug build constant file and add debug const
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- log.go
+++ log.go
@@ -2,16 +2,61 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// +build !debug
-
 package ti
 
 import (
+	"fmt"
+	"log"
 	"time"
+
+	"github.com/davecgh/go-spew/spew"
+	colorpkg "github.com/fatih/color"
 )
 
-func Debug(a ...interface{})                 {}
-func Debugf(format string, a ...interface{}) {}
-func Debugln(a ...interface{})               {}
-func Dump(a ...interface{})                  {}
-func Profile(name string, now time.Time)     {}
+const (
+	debug = false
+)
+
+func init() {
+	log.SetFlags(log.Lshortfile)
+	log.SetPrefix(colorpkg.HiBlackString("[DEBUG]: "))
+}
+
+// Debug prints debug log.
+// Arguments are handled in the manner of fmt.Print.
+func Debug(a ...interface{}) {
+	if debug {
+		log.Output(2, fmt.Sprint(a...))
+	}
+}
+
+// Debugf prints debug log.
+// Arguments are handled in the manner of fmt.Printf.
+func Debugf(format string, a ...interface{}) {
+	if debug {
+		log.Output(2, fmt.Sprintf(format, a...))
+	}
+}
+
+// Debugln prints debug log.
+// Arguments are handled in the manner of fmt.Println.
+func Debugln(a ...interface{}) {
+	if debug {
+		log.Output(2, fmt.Sprintln(a...))
+	}
+}
+
+// Dump dumps a and prints debug log.
+// Arguments are handled in the manner of fmt.Print.
+func Dump(a ...interface{}) {
+	if debug {
+		log.Output(2, spew.Sdump(a...))
+	}
+}
+
+// Profile the end time of the function.
+func Profile(name string, now time.Time) {
+	if debug {
+		log.Output(2, fmt.Sprintf("%s: %fsec", name, time.Since(now).Seconds()))
+	}
+}
