1ce58c6fd2e21ace6a3bf21b9a5113ce76908285
Koichi Shiraishi <zchee.io@gmail.com>
2017-06-27 11:06:23 +0900

    io,rlimit: split rlimitNoFileSize function, support windows
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- io.go
+++ io.go
@@ -7,7 +7,6 @@ package ti
 import (
 	"os"
 	"path/filepath"
-	"syscall"
 
 	xdgbasedir "github.com/zchee/go-xdgbasedir"
 )
@@ -20,20 +19,6 @@ var (
 	// IgnoreFile returns the ti ignore file path.
 	IgnoreFile = filepath.Join(ConfigHome, "ignore")
 )
-
-// rlimitNoFileSize returns the RLIMIT_NOFILE size.
-func rlimitNoFileSize() uint64 {
-	const raceMax = 8192
-	size := uint64(256) // 256 is macOS default size
-	rlimit := syscall.Rlimit{}
-	if err := syscall.Getrlimit(syscall.RLIMIT_NOFILE, &rlimit); err == nil {
-		if raceMax < rlimit.Cur {
-			size = raceMax
-		}
-		size = rlimit.Cur
-	}
-	return size
-}
 
 // FileDescriptor opens path and returns the file descriptor if path is non-nil.
 // If path is empty, returns the stdin file descriptor.
---
+++ rlimit_unix.go
@@ -0,0 +1,25 @@
+// Copyright 2017 The the_titanium_searcher Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// +build !windows
+
+package ti
+
+import (
+	"syscall"
+)
+
+// rlimitNoFileSize returns the RLIMIT_NOFILE size.
+func rlimitNoFileSize() uint64 {
+	const raceMax = 8192
+	size := uint64(256) // 256 is macOS default size
+	rlimit := syscall.Rlimit{}
+	if err := syscall.Getrlimit(syscall.RLIMIT_NOFILE, &rlimit); err == nil {
+		if raceMax < rlimit.Cur {
+			size = raceMax
+		}
+		size = rlimit.Cur
+	}
+	return size
+}
